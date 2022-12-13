01ace4d9ac0078ca490fa4485e68ec083d38100b
GARANTO <antonin.garanto@cgi.com>
2022-12-13 18:30:10 +0100

    feat(instances): toolbar, filter btn

--- src/Components/Instances.js
+++ src/Components/Instances.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./Instances.css";
 
 function NewInstanceButton({ className }) {
@@ -7,25 +8,50 @@ function NewInstanceButton({ className }) {
 }
 
 function FilterInstanceButton({ className }) {
+    function sortByCiv(e){
+        setExpend(false);
+        console.log(e.target.innerHTML);
+    }
+
+    function sortByMap(e){
+        setExpend(false);
+        console.log(e.target.innerHTML);
+    }
+
+    function sortByGoal(e){
+        setExpend(false);
+        console.log(e.target.innerHTML);
+    }
+
+    const [expend, setExpend] = useState(false);
     return (
         <div>
-            <input type="checkbox" id="filter-checkbox"/>
-            <label  className="filter-label" for="filter-checkbox">Filter By</label>
+            <input
+                type="checkbox" id="filter-checkbox" checked={expend} 
+                onChange={(e) => {setExpend(e.target.checked ?  true : false)}}
+            />
+            <label className="filter-label" for="filter-checkbox">Filter by</label>
             <div className="dropdown-options">
-                <div className="options-civ">Civilization</div>
-                <div className="dropdown-civs">
-                    <a href="#">Dido</a>
-                    <a href="#">Trajan</a>
+                
+                <input type="checkbox" id="civ-checkbox"/>
+                <label className="options-civ dropdown-select" for="civ-checkbox">Civilization</label>
+                <div className="dropdowns  dropdown-civs">
+                    <label className="dropdown-select" onClick={sortByCiv}>Dido</label>
+                    <label className="dropdown-select" onClick={sortByCiv}>Trajan</label>
                 </div>
-                <div className="options-map">Map</div>
-                <div className="dropdown-maps">
-                    <a href="#">Seven Seas</a>
-                    <a href="#">Pangea</a>
+
+                <input type="checkbox" id="map-checkbox"/>
+                <label className="options-map dropdown-select" htmlFor="map-checkbox">Map</label>
+                <div className="dropdowns dropdown-maps">
+                    <label className="dropdown-select" onClick={sortByMap}>Seven Seas</label>
+                    <label className="dropdown-select" onClick={sortByMap}>Pangea</label>
                 </div>
-                <div className="options-goal">Goal</div>
-                <div className="dropdown-goals">
-                    <a href="#">Science Victory</a>
-                    <a href="#">Cultural Victory</a>
+
+                <input type="checkbox" id="goal-checkbox"/>
+                <label className="options-goal dropdown-select" for="goal-checkbox">Goal</label>
+                <div className="dropdowns dropdown-goals">
+                    <label className="dropdown-select" onClick={sortByGoal}>Science Victory</label>
+                    <label className="dropdown-select" onClick={sortByGoal}>Cultural Victory</label>
                 </div>
             </div>
         </div>
@@ -35,7 +61,6 @@ function FilterInstanceButton({ className }) {
 function ListInstances() {
     return (
         <div>
-
         </div>
     );
 }
@@ -44,7 +69,7 @@ export function Instances() {
     return (
         <div className="instances">
             <div className="utils">
-                    <NewInstanceButton className={"button"}/>
+                    <NewInstanceButton className={"instance-button"}/>
                     <FilterInstanceButton className={"filter-button"}/>
             </div>
             <div className="list-instances">
